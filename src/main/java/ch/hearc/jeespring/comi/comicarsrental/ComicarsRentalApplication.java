71a223f23b682053b2cd24a6ba5b50c0f572b2e5
Alessio Comi <alessio.comi@he-arc.ch>
2024-01-27 14:35:42 +0100

    upload project basis

---
+++ src/main/java/ch/hearc/jeespring/comi/comicarsrental/ComicarsRentalApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.jeespring.comi.comicarsrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicarsRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicarsRentalApplication.class, args);
+	}
+
+}
